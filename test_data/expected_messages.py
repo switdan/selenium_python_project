49a006afa7a93bef3942f0b4a3e7192db56c8857
Daniel Świtała <switala.daniel@gmail.com>
2026-04-20 17:24:45 +0200

    expected messages moved to the new file

---
+++ test_data/expected_messages.py
@@ -0,0 +1,3 @@
+class ExpectedMessages:
+    REGISTRATION_HEADER = "CREATE AN ACCOUNT"
+    REGISTRATION_BANNER = "Your account has been created."
--- tests/test_registration.py
+++ tests/test_registration.py
@@ -2,6 +2,7 @@ import pytest
 
 from flows.registration_flow import RegistrationFlow
 from test_data.registration_data import RegistrationDataGenerator
+from test_data.expected_messages import ExpectedMessages
 
 class TestPositiveScenarios:
 
@@ -9,14 +10,14 @@ class TestPositiveScenarios:
     @pytest.mark.positive
     @pytest.mark.debug
     def test_registration(self, driver):
-        flow = RegistrationFlow(driver)
         data = RegistrationDataGenerator()
+        flow = RegistrationFlow(driver)
 
         flow.start_registration(data.EMAIL)
-        assert flow.create_an_account_page.get_h1_title() == "CREATE AN ACCOUNT", "Registration page title didn't match"
+        assert flow.create_an_account_page.get_h1_title() == ExpectedMessages.REGISTRATION_HEADER, "Registration page title didn't match"
 
         flow.complete_registration(data.GENDER, data.FIRST_NAME, data.LAST_NAME, data.PASSWORD, data.DAY, data.MONTH, data.YEAR)
-        assert flow.my_account_page.get_green_banner_text() == "Your account has been created.", "Green banner text didn't match"
+        assert flow.my_account_page.get_green_banner_text() == ExpectedMessages.REGISTRATION_BANNER, "Green banner text didn't match"
 
 
 
